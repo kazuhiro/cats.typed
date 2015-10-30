361cf9e5fcf829c4259c0ab6b0e49d034ea4f2da
Piotr Jarzemski <jarzemski.piotr@gmail.com>
2015-10-30 12:39:36 +0100

    Bump up dependencies to the newest versions

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "BSD (2 Clause)"
             :url "http://opensource.org/licenses/BSD-2-Clause"}
   :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]
-                 [org.clojure/core.typed "0.3.11"]
-                 [funcool/cats "0.6.1" :scope "provided"]]
+                 [org.clojure/core.typed "0.3.14"]
+                 [funcool/cats "1.0.0" :scope "provided"]]
   :plugins [[lein-typed "0.3.5"]]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
