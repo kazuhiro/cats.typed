7c346d0734c11ee841f1caae78849b3cb411bc7f
Piotr Jarzemski <jarzemski.piotr@gmail.com>
2016-01-31 16:38:19 +0100

    Bump up clojure, core.typed, and cats library

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject cats.typed "0.1.0-SNAPSHOT"
+(defproject cats.typed "0.1.1-SNAPSHOT"
   :description "org.clojure/core.typed definitions for funcool/cats library"
   :url "https://github.com/kazuhiro/cats.typed"
   :license {:name "BSD (2 Clause)"
             :url "http://opensource.org/licenses/BSD-2-Clause"}
-  :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]
-                 [org.clojure/core.typed "0.3.14"]
-                 [funcool/cats "1.0.0" :scope "provided"]]
+  :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
+                 [org.clojure/core.typed "0.3.20"]
+                 [funcool/cats "1.2.1" :scope "provide"]] ;; 1.2.1
   :plugins [[lein-typed "0.3.5"]]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
